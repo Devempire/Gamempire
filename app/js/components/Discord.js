b9a2a1ce33ee8fcb92f8538a6fd09d2ab55ef137
mmmmeng3 <rain950617@gmail.com>
2017-02-06 23:19:23 -0500

    my bad to remove discord.js,bring it back

---
+++ app/js/components/Discord.js
@@ -0,0 +1,12 @@
+module.exports = global.Discord = React.createClass({
+ 
+ 	render() {
+ 		return <div className="discordFrame">
+       <script>
+       ipcRenderer.send('disable-x-frame', webview.partition);
+       </script>
+ 			<webview className="discordFrame" src="https://discordapp.com/login"></webview>
+     </div>;
+ 
+ 	}
+ });
