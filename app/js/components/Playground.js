e199138b4057dd7f3688e37f7ce1a4e9c51a4868
TheComeUp <borys.kotanski@gmail.com>
2017-02-10 05:07:08 -0500

    Just playing
    in the playground

--- app/js/components/Playground.js
+++ app/js/components/Playground.js
@@ -40,8 +40,21 @@ module.exports = global.Playground = React.createClass({
   },
 
   toggleSoundcloud() {
-    $( "#Soundcloud" ).toggle();;
+    $( "#Soundcloud" ).toggle();
   },
+
+  toggleMessenger() {
+    $( "#Messenger" ).toggle();
+  },
+
+  toggleSteamChat() {
+    $( "#SteamChat" ).toggle();
+  },
+
+  toggleGooglePlay() {
+    $( "#GooglePlay" ).toggle();
+  },
+
 
   render() {
     // Set Titles
@@ -57,7 +70,7 @@ module.exports = global.Playground = React.createClass({
           breakpoints={{lg: 1042, md: 996, sm: 768, xs: 480, xxs: 0}}
           cols={{lg: 17, md: 12, sm: 8, xs: 4, xxs: 2}}>
 
-          <div key={"1"} data-grid={{x: 0, y: 0, w: 3, h: 2, static: true}} className="widgetFrame noselect" id="settings">
+          <div key={"1"} data-grid={{x: 0, y: 0, w: 3, h: 3, static: true}} className="widgetFrame noselect" id="settings">
             <span className="widgetTitle noselect">Settings</span>
 
             <label className= "label" htmlFor="sel_soundcloud">Soundcloud</label>
@@ -78,6 +91,33 @@ module.exports = global.Playground = React.createClass({
                 </label>
             </div><br/>
 
+            <label className= "label" htmlFor="sel_messenger">Messenger</label>
+            <div className="onoffswitch">
+                <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="sel_messenger" onChange={this.toggleMessenger} defaultChecked/>
+                <label className="onoffswitch-label" htmlFor="sel_messenger">
+                    <span className="onoffswitch-inner"></span>
+                    <span className="onoffswitch-switch"></span>
+                </label>
+            </div><br/>
+
+            <label className= "label" htmlFor="sel_steamchat">Steam Chat</label>
+            <div className="onoffswitch">
+                <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="sel_steamchat" onChange={this.toggleSteamChat} defaultChecked/>
+                <label className="onoffswitch-label" htmlFor="sel_steamchat">
+                    <span className="onoffswitch-inner"></span>
+                    <span className="onoffswitch-switch"></span>
+                </label>
+            </div><br/>
+
+            <label className= "label" htmlFor="sel_GooglePlay">Google Play</label>
+            <div className="onoffswitch">
+                <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="sel_GooglePlay" onChange={this.toggleGooglePlay} defaultChecked/>
+                <label className="onoffswitch-label" htmlFor="sel_GooglePlay">
+                    <span className="onoffswitch-inner"></span>
+                    <span className="onoffswitch-switch"></span>
+                </label>
+            </div><br/>
+
           </div>
 
           <div key={"2"} data-grid={{x: 3, y: 0, w: 5, h: 3}} className="widgetFrame" id="Discord">
@@ -92,6 +132,26 @@ module.exports = global.Playground = React.createClass({
             <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
             <webview className="widget" src="https://soundcloud.com/charts/top" ></webview>
           </div>
+
+          <div key={"4"} data-grid={{x: 0, y: 3, w: 8, h: 3}} className="widgetFrame" id="Messenger">
+            <p className="widgetTitle noselect">Messenger</p>
+            <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
+            <webview className="widget" src="https://www.messenger.com/login/" ></webview>
+          </div>
+
+          <div key={"5"} data-grid={{x: 8, y: 3, w: 8, h: 3}} className="widgetFrame" id="SteamChat">
+            <p className="widgetTitle noselect">Steam Chat</p>
+            <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
+            <webview className="widget" src="https://steamcommunity.com/chat" ></webview>
+          </div>
+
+          <div key={"6"} data-grid={{x: 0, y: 6, w: 10, h: 3}} className="widgetFrame" id="GooglePlay">
+            <p className="widgetTitle noselect">Google Play</p>
+            <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
+            <webview className="widget" src="https://play.google.com/music/listen?authuser#/home" ></webview>
+          </div>
+
+
 {
 /*
           <div key={"4"} data-grid={{x: 0, y: 2, w: 8, h: 2}} className="widgetFrame" id="Harthstone">
