0b99640ad04614fd1d97df0d8db274df69071519
TheComeUp <borys.kotanski@gmail.com>
2017-02-17 11:46:27 -0500

    no drag inside widget fixed

--- app/js/components/Playground.js
+++ app/js/components/Playground.js
@@ -2,9 +2,6 @@ var WidthProvider = require('react-grid-layout').WidthProvider;
 var ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
 ResponsiveReactGridLayout = WidthProvider(ResponsiveReactGridLayout);
 
-var WidthProvider = require('react-grid-layout').WidthProvider;
-var ReactGridLayout = require('react-grid-layout');
-ReactGridLayout = WidthProvider(ReactGridLayout);
 
 module.exports = global.Playground = React.createClass({
   mixins: [PureRenderMixin],
@@ -14,7 +11,6 @@ module.exports = global.Playground = React.createClass({
       className: "layout",
       cols: {lg: 17, md: 12, sm: 12, xs: 4, xxs: 4},
       rowHeight: 30,
-
       verticalCompact: true
     };
     //this.handleChildUnmount = this.handleChildUnmount.bind(this);
@@ -66,7 +62,7 @@ module.exports = global.Playground = React.createClass({
     //Set Dashbaord as active in menu
     $( "#_Playground" ).addClass('active');
     return <div>
-    <ResponsiveReactGridLayout className="layout" layouts={this.state.layouts} 
+    <ResponsiveReactGridLayout draggableCancel={".widget"} className="layout" layouts={this.state.layouts}
           breakpoints={{lg: 1042, md: 996, sm: 768, xs: 480, xxs: 0}}
           cols={{lg: 17, md: 12, sm: 8, xs: 4, xxs: 2}}>
 
@@ -120,17 +116,17 @@ module.exports = global.Playground = React.createClass({
 
           </div>
 
-          <div key={"2"} data-grid={{x: 3, y: 0, w: 5, h: 3, draggableCancel: '.widget', draggableHandle: '.widgetTitle noselect'}} className="widgetFrame" id="Discord">
-            <span className="widgetTitle noselect">Discord</span>
+          <div key={"2"} data-grid={{x: 3, y: 0, w: 5, h: 3}} className="widgetFrame"  id="Discord">
+            <span className="widgetTitle">Discord</span>
             <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
-            <webview className="widget" src="https://discordapp.com/login" ></webview>
+            <webview className="widget" src="https://discordapp.com/login"></webview>
           </div>
 
 
           <div key={"3"} data-grid={{x: 8, y: 0, w: 8, h: 3}} className="widgetFrame" id="Soundcloud">
             <p className="widgetTitle noselect">Soundcloud</p>
             <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
-            <webview className="widget" src="https://soundcloud.com/charts/top" ></webview>
+            <webview className="widget" src="https://soundcloud.com/charts/top" data-grid={{static: true}}></webview>
           </div>
 
           <div key={"4"} data-grid={{x: 0, y: 3, w: 8, h: 3}} className="widgetFrame" id="Messenger">
@@ -150,7 +146,6 @@ module.exports = global.Playground = React.createClass({
             <script>ipcRenderer.send('disable-x-frame', webview.partition);</script>
             <webview className="widget" src="https://play.google.com/music/listen?authuser#/home" ></webview>
           </div>
-
 
 {
 /*
