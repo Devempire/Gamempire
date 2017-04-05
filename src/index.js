8a0cc9830b463bcc99aaf725be9d1fe61ff3588c
MaxJeong <max.jeong@mail.utoronto.ca>
2017-04-04 23:57:17 -0400

    removed some code

--- src/index.js
+++ src/index.js
@@ -14,8 +14,6 @@ var ipc = electron.ipcMain;
 const path = require('path')
 var iconPath = __dirname + '../../app/img/logo.ico';
 
-const {systemPreferences} = require('electron')
-
 global.sharedObject = {
   token: null,
   username:null,
@@ -32,15 +30,6 @@ global.sharedObject = {
 let mainWindow
 
 let createWindow = () => {
-  var backgroundElements = [
-      "body",
-      "html",
-      ".overlay",
-      "table tbody",
-      "table tfoot",
-      "table thead"
-  ];
-
   // Create the browser window.
   mainWindow  = new BrowserWindow({
     minHeight: 480,
