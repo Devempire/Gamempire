bc303dcdda53a4e1f286787659f5c5948d705d77
Dillon Newell <dillon_newell@live.ca>
2017-05-31 02:34:16 -0400

    Split off helpers

--- app/js/components/GDQ/GDQEvent.js
+++ app/js/components/GDQ/GDQEvent.js
@@ -1,5 +1,33 @@
+import formatMoney from 'accounting';
+import formatDate from '../../Helpers/Formatters.js';
+
 export class GDQEvent {
-  constructor (id) {
+  constructor (id, name, shortName, date, target, raised) {
     this.id = id;
+    this.name = name;
+    this.shortName = shortName;
+    this.date = new Date(date);
+    this.target = parseInt(target);
+    this.raised = parseInt(raised);
+  }
+
+  toString () {
+    return this.shortName;
+  }
+
+  statusString () {
+    if (this.date > new Date()) {
+      return 'Upcoming: Starts on ' + formatDate(this.date);
+    } else {
+      return 'Started on ' + formatDate(this.date);
+    }
+  }
+
+  donationString () {
+    if (this.raised >= this.target) {
+      return 'Met! Raised ' + formatMoney(this.raised) + '/' + formatMoney(this.target);
+    } else {
+      return 'Raised ' + formatMoney(this.raised) + '/' + formatMoney(this.target);
+    }
   }
 }
