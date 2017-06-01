100470cecef497e060ce545a2877b43f9d820d5a
Dillon Newell <dillon_newell@live.ca>
2017-06-01 06:56:13 -0400

    Work on objectivity

--- app/js/components/GDQ/GDQManager.js
+++ app/js/components/GDQ/GDQManager.js
@@ -5,16 +5,18 @@ const gdqapi = 'https://gamesdonequick.com/tracker/search';
 
 let instance = null;
 
-export class GDQManager {
+export class GDQManager extends React.Component {
   static Instance () {
     if (instance == null) {
-      console.log('Creating GDQManager instance')
+      console.log('Creating GDQManager instance');
       instance = new GDQManager();
     }
     return instance;
   }
 
   constructor () {
+    super();
+
     this._events = [];
     this._runners = [];
     this._eventsLoaded = false;
@@ -44,11 +46,11 @@ export class GDQManager {
           var target = evnt.fields.targetamount;
           var raised = evnt.fields.amount;
           var name = evnt.fields.name;
-          that._events.push(new GDQEvent(id));
+          that._events.push(new GDQEvent(id, name, shortName, date, target, raised));
         });
-        that._events.slice(0).sort(function (a, b) {
+        that.setState({_events: that._events.slice(0).sort(function (a, b) {
           return b.date - a.date;
-        });
+        })});
         that._eventsLoaded = true;
         console.log('Events loaded');
         console.log(that._events);
@@ -56,6 +58,12 @@ export class GDQManager {
         console.log('Error loading events');
         console.log(err);
       }
+    });
+  }
+
+  eventFromID (id) {
+    return this._events.find(function (event) {
+      return (event.id == id);
     });
   }
 }
